52f19fdae72b778584bcb7d8201e85fc2f3e0eff
Shashank Dewangan <21052358@kiit.ac.in>
2023-10-16 00:28:24 +0530

    Sidebar Updation

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,19 +1,28 @@
 "use client"
 
 import {Plus} from "lucide-react";
+import {ActionToolTip} from "@/components/ui/action-tooltip";
 
 export const NavigationAction = () => {
     return (
         <div>
-            <button className="group">
-                <div
-                    className="flex mx-3 h-[48px] w-[48px] rounded-[24px]
+            <ActionToolTip
+                label="Add a server"
+                side="right"
+                align="center"
+            >
+                <button
+                    className="group"
+                >
+                    <div
+                        className="flex mx-3 h-[48px] w-[48px] rounded-[24px]
                     group-hover:rounded-[16px] transition-all
                     overflow-hidden items-center justify-center bg-background
                     dark:bg-neutral-700 group-hover:bg-blue-500">
-                    <Plus className="group-hover:text-white transition text-blue-500" size={25}/>
-                </div>
-            </button>
+                        <Plus className="group-hover:text-white transition text-blue-500" size={25}/>
+                    </div>
+                </button>
+            </ActionToolTip>
         </div>
     )
 }
